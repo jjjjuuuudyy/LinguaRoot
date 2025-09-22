0e2ea09fcc823ba43d9a90e302c47046f0caddd2
jjjjuuuudyy <140327729+jjjjuuuudyy@users.noreply.github.com>
2025-09-22 20:13:47 +0800

    revise

--- start_project.bat
+++ start_project.bat
@@ -1,12 +1,12 @@
 @echo off
 echo run Django server
-cd D:\project\my-react-app\my-react-app\backend
+cd D:\project\LinguaRoot\backend
 start python manage.py runserver
 
 echo run FastAPI server
-cd D:\project\my-react-app\my-react-app\backend
+cd D:\project\LinguaRoot\backend
 start uvicorn fastAPI.main:app --host 127.0.0.1 --port 8001 --reload
 
 echo run frontend React + Vite
-cd D:\project\my-react-app\my-react-app\
+cd D:\project\LinguaRoot\
 start npm run dev
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'frontend',
   base: '/LinguaRoot/',
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
 })
